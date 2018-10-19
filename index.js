9e03f85a8c075dbc198c38c80300805b5cc6e8f0
Kalin Krustev <kalin.krustev@gmail.com>
2018-10-19 13:54:17 +0300

    tests: upgrade ut-tools

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function tagsDecode(emvString, result, dolIdx) {
             emvString = emvString.substr(len * 2);
         }
     }
-    let constructedTagByte = (new Buffer(tag, 'hex')).slice(0, 1)[0];
+    let constructedTagByte = (Buffer.from(tag, 'hex')).slice(0, 1)[0];
     if ((constructedTagByte & 32) === 32) {
         result[tagTranslated].val = (isDol ? tagsDecode(val, {}, 1) : tagsDecode(val, {}, (dolIdx ? dolIdx + 1 : dolIdx)));
     } else {
@@ -232,7 +232,7 @@ function tagsEncode(emvTags) {
         let tagLength = getValueHexLength(tagObj);
         let tagValue = tagObj.val;
 
-        let constructedTagByte = (new Buffer(tagTranslated, 'hex')).slice(0, 1)[0];
+        let constructedTagByte = (Buffer.from(tagTranslated, 'hex')).slice(0, 1)[0];
         if ((constructedTagByte & 32) === 32) {
             tagValue = tagsEncode(tagObj.val);
         }
